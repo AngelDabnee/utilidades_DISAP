4fc0948e9cfb4378ad7f0b37ebbfec39d46cca98
AngelDabnee <a22311046@uthermosillo.edu.mx>
2023-10-30 15:34:57 -0700

    first commit

---
+++ app/src/main/java/AngelDabnee/com/SplashActivity.kt
@@ -0,0 +1,24 @@
+package AngelDabnee.com
+
+import android.content.Intent
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val logo = findViewById<ImageView>(R.id.imagenLogo)
+
+        logo.setOnClickListener()
+        {
+            var i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            finish()
+            Toast.makeText(this,"Pique al logo",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
